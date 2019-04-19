6d29a4da68c1889c63dd8ae66880f03ee1bee85a
Herman White <phoenixquint@gmail.com>
2019-04-18 22:08:23 -0400

    Added form validation for signupform

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Redirect } from "react-router-dom";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Alert } from "react-bootstrap";
 
 class SignupForm extends Component {
   constructor() {
@@ -22,23 +22,43 @@ class SignupForm extends Component {
   }
   handleSubmit(event) {
     event.preventDefault();
-    // TODO - validate!
-    axios
-      .post("/auth/signup", {
-        username: this.state.username,
-        password: this.state.password
-      })
-      .then(response => {
-        console.log(response);
-        if (!response.data.error) {
-          console.log("User created");
-          this.setState({
-            redirectTo: "/login"
-          });
-        } else {
-          console.log("Error: Duplicate user");
+    // email validation regex
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (re.test(String(this.state.username).toLowerCase())) {
+      axios
+        .post("/auth/signup", {
+          username: this.state.username.toLowerCase(),
+          password: this.state.password
+        })
+        .then(response => {
+          console.log(response);
+          if (!response.data.error) {
+            // User successfully created
+            this.setState({
+              redirectTo: "/login"
+            });
+          } else {
+            // Duplicate user found
+            this.setState({
+              error: {
+                cause: `Username ${this.state.username} already exists!`,
+                solution: `Please use a different username or login if username ${
+                  this.state.username
+                } is yours.`
+              }
+            });
+          }
+        });
+    } else {
+      // If email is invalid
+      this.setState({
+        error: {
+          cause: "Invalid Username",
+          solution: "Username must be valid - example@example.com"
         }
       });
+    }
   }
   render() {
     if (this.state.redirectTo) {
@@ -48,6 +68,13 @@ class SignupForm extends Component {
       <div className="signup-form-container">
         <Form className="form-controls">
           <Form.Group>
+            {!!this.state.error && (
+              // Only shows if error exists
+              <Alert variant="danger">
+                <Alert.Heading>{this.state.error.cause}</Alert.Heading>
+                <p>{this.state.error.solution}</p>
+              </Alert>
+            )}
             <Form.Label>Email Address</Form.Label>
             <Form.Control
               type="text"
@@ -74,6 +101,14 @@ class SignupForm extends Component {
               name="confirmPassword"
               value={this.state.confirmPassword}
               onChange={this.handleChange}
+              isValid={
+                this.state.password.length &&
+                this.state.password === this.state.confirmPassword
+              }
+              isInvalid={
+                this.state.password.length &&
+                this.state.password !== this.state.confirmPassword
+              }
             />
           </Form.Group>
           <Button variant="primary" type="submit" onClick={this.handleSubmit}>
