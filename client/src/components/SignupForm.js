b17ab10cebdbc9f1076ccbac6355920424da69f2
Herman White <phoenixquint@gmail.com>
2019-04-23 00:02:33 -0400

    Additional heroku changes

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -23,6 +23,7 @@ class SignupForm extends Component {
   handleSubmit(event) {
     event.preventDefault();
     // email validation regex
+    // eslint-disable-next-line
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (re.test(String(this.state.username).toLowerCase())) {
